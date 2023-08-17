655c53c216331ea02da6a232bcace879bf46b1b6
Des <desireemora@mail.usf.edu>
2023-08-16 20:09:41 -0500

    Preliminary webscraper can grab some comic titles

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Ethical Comic Crawler
+Using `robotexclusionrulesparser` ensures that I am following all the rules defined by the robots.txt file of any website being scraped.
+
+`pip install -r requirements.txt`
---
+++ scrape.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+from robotexclusionrulesparser import RobotFileParserLookalike as RobotParser
+
+BASE_URL = 'https://www.mycomicshop.com/search?TID=25730429'
+
+def is_allowed_to_scrape(url, user_agent):
+    rp = RobotParser()
+    rp.set_url(url + "robots.txt")
+    rp.read()
+    return rp.can_fetch(user_agent, url)
+
+def scrape_mycomicshop():
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+
+    if not is_allowed_to_scrape(BASE_URL, user_agent):
+        print("We're not allowed to scrape this website based on its robots.txt.")
+        return
+
+    headers = {
+        'User-Agent': user_agent
+    }
+
+    response = requests.get(BASE_URL, headers=headers)
+
+    if response.status_code != 200:
+        print("Failed to retrieve the webpage.")
+        return
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    #print(soup)
+
+    # Assuming the comic titles are in an 'h2' tag (this might change depending on the website's structure)
+    #comic_titles = soup.find_all('li', class_='issue').find('div',class_='title') # This class might need adjustment based on the site's structure
+    comic_titles = soup.select('li.issue div.title')
+
+    for index, title in enumerate(comic_titles, 1):
+        print(f"{index}. {title.text.strip()}")
+
+if __name__ == '__main__':
+    scrape_mycomicshop()
